fc17982e22c7dad8f13eeae333750eb129d41eac
Aaron Kriegman <aaronkplus2@gmail.com>
2024-07-09 22:26:37 -0400

    refactored keymapping

--- tonnetz.cpp
+++ tonnetz.cpp
@@ -4,45 +4,60 @@
 #include <unistd.h>
 #include <unordered_map>
 
-const std::unordered_map<__u16, unsigned char> tonnetz = {
+using namespace std;
 
-    {KEY_ESC, 109},       {KEY_F1, 104},       {KEY_F2, 99},
-    {KEY_F3, 94},         {KEY_F4, 89},        {KEY_F5, 84},
-    {KEY_F6, 79},         {KEY_F7, 74},        {KEY_F8, 69},
-    {KEY_F9, 64},         {KEY_F10, 59},       {KEY_F11, 54},
-    {KEY_F12, 49},        {KEY_SYSRQ, 44},     {KEY_SCROLLLOCK, 39},
-    {KEY_PAUSE, 34},
+const unordered_map<__u16, pair<int, int>> tonnetz = {
 
-    {KEY_GRAVE, 110},     {KEY_1, 105},        {KEY_2, 100},
-    {KEY_3, 95},          {KEY_4, 90},         {KEY_5, 85},
-    {KEY_6, 80},          {KEY_7, 75},         {KEY_8, 70},
-    {KEY_9, 65},          {KEY_0, 60},         {KEY_MINUS, 55},
-    {KEY_EQUAL, 50},      {KEY_BACKSPACE, 45}, {KEY_INSERT, 40},
-    {KEY_HOME, 35},       {KEY_PAGEUP, 30},
+    {KEY_ESC, {5, 1}},         {KEY_F1, {5, 2}},
+    {KEY_F2, {5, 3}},          {KEY_F3, {5, 4}},
+    {KEY_F4, {5, 5}},          {KEY_F5, {5, 6}},
+    {KEY_F6, {5, 7}},          {KEY_F7, {5, 8}},
+    {KEY_F8, {5, 9}},          {KEY_F9, {5, 10}},
+    {KEY_F10, {5, 11}},        {KEY_F11, {5, 12}},
+    {KEY_F12, {5, 13}},        {KEY_SYSRQ, {5, 14}},
+    {KEY_SCROLLLOCK, {5, 15}}, {KEY_PAUSE, {5, 16}},
 
-    {KEY_TAB, 101},       {KEY_Q, 96},         {KEY_W, 91},
-    {KEY_E, 86},          {KEY_R, 81},         {KEY_T, 76},
-    {KEY_Y, 71},          {KEY_U, 66},         {KEY_I, 61},
-    {KEY_O, 56},          {KEY_P, 51},         {KEY_LEFTBRACE, 46},
-    {KEY_RIGHTBRACE, 41}, {KEY_BACKSLASH, 36}, {KEY_DELETE, 31},
-    {KEY_END, 26},        {KEY_PAGEDOWN, 21},
+    {KEY_GRAVE, {4, 0}},       {KEY_1, {4, 1}},
+    {KEY_2, {4, 2}},           {KEY_3, {4, 3}},
+    {KEY_4, {4, 4}},           {KEY_5, {4, 5}},
+    {KEY_6, {4, 6}},           {KEY_7, {4, 7}},
+    {KEY_8, {4, 8}},           {KEY_9, {4, 9}},
+    {KEY_0, {4, 10}},          {KEY_MINUS, {4, 11}},
+    {KEY_EQUAL, {4, 12}},      {KEY_BACKSPACE, {4, 13}},
+    {KEY_INSERT, {4, 14}},     {KEY_HOME, {4, 15}},
+    {KEY_PAGEUP, {4, 16}},
 
-    {KEY_CAPSLOCK, 92},   {KEY_A, 87},         {KEY_S, 82},
-    {KEY_D, 77},          {KEY_F, 72},         {KEY_G, 67},
-    {KEY_H, 62},          {KEY_J, 57},         {KEY_K, 52},
-    {KEY_L, 47},          {KEY_SEMICOLON, 42}, {KEY_APOSTROPHE, 37},
-    {KEY_ENTER, 32},
+    {KEY_TAB, {3, 1}},         {KEY_Q, {3, 2}},
+    {KEY_W, {3, 3}},           {KEY_E, {3, 4}},
+    {KEY_R, {3, 5}},           {KEY_T, {3, 6}},
+    {KEY_Y, {3, 7}},           {KEY_U, {3, 8}},
+    {KEY_I, {3, 9}},           {KEY_O, {3, 10}},
+    {KEY_P, {3, 11}},          {KEY_LEFTBRACE, {3, 12}},
+    {KEY_RIGHTBRACE, {3, 13}}, {KEY_BACKSLASH, {3, 14}},
+    {KEY_DELETE, {3, 15}},     {KEY_END, {3, 16}},
+    {KEY_PAGEDOWN, {3, 17}},
 
-    {KEY_LEFTSHIFT, 83},  {KEY_Z, 78},         {KEY_X, 73},
-    {KEY_C, 68},          {KEY_V, 63},         {KEY_B, 58},
-    {KEY_N, 53},          {KEY_M, 48},         {KEY_COMMA, 43},
-    {KEY_DOT, 38},        {KEY_SLASH, 33},     {KEY_RIGHTSHIFT, 28},
-    {KEY_UP, 23},
+    {KEY_CAPSLOCK, {2, 2}},    {KEY_A, {2, 3}},
+    {KEY_S, {2, 4}},           {KEY_D, {2, 5}},
+    {KEY_F, {2, 6}},           {KEY_G, {2, 7}},
+    {KEY_H, {2, 8}},           {KEY_J, {2, 9}},
+    {KEY_K, {2, 10}},          {KEY_L, {2, 11}},
+    {KEY_SEMICOLON, {2, 12}},  {KEY_APOSTROPHE, {2, 13}},
+    {KEY_ENTER, {2, 14}},
 
-    {KEY_LEFTCTRL, 79},   {KEY_LEFTMETA, 74},  {KEY_LEFTALT, 69},
-    {KEY_SPACE, 0},       {KEY_RIGHTALT, 34},  {KEY_COMPOSE, 29},
-    {KEY_RIGHTCTRL, 24},  {KEY_LEFT, 19},      {KEY_DOWN, 14},
-    {KEY_RIGHT, 9},
+    {KEY_LEFTSHIFT, {1, 3}},   {KEY_Z, {1, 4}},
+    {KEY_X, {1, 5}},           {KEY_C, {1, 6}},
+    {KEY_V, {1, 7}},           {KEY_B, {1, 8}},
+    {KEY_N, {1, 9}},           {KEY_M, {1, 10}},
+    {KEY_COMMA, {1, 11}},      {KEY_DOT, {1, 12}},
+    {KEY_SLASH, {1, 13}},      {KEY_RIGHTSHIFT, {1, 14}},
+    {KEY_UP, {1, 15}},
+
+    {KEY_LEFTCTRL, {0, 3}},    {KEY_LEFTMETA, {0, 4}},
+    {KEY_LEFTALT, {0, 5}},     {KEY_SPACE, {0, 6}},
+    {KEY_RIGHTALT, {0, 12}},   {KEY_COMPOSE, {0, 13}},
+    {KEY_RIGHTCTRL, {0, 14}},  {KEY_LEFT, {0, 15}},
+    {KEY_DOWN, {0, 16}},       {KEY_RIGHT, {0, 17}},
 };
 
 int main() {
@@ -51,8 +66,7 @@ int main() {
 
   status = snd_rawmidi_open(nullptr, &midi_out, "virtual", SND_RAWMIDI_SYNC);
   if (status < 0) {
-    std::cerr << "Error opening MIDI output: " << snd_strerror(status)
-              << std::endl;
+    cerr << "Error opening MIDI output: " << snd_strerror(status) << endl;
     return 1;
   }
 
@@ -72,7 +86,8 @@ int main() {
       }
 
       if (tonnetz.find(event.code) != tonnetz.end()) {
-        note[1] = tonnetz.at(event.code);
+        pair<int, int> coords = tonnetz.at(event.code);
+        note[1] = coords.first * 4 + coords.second * 7;
       } else {
         continue;
       }
